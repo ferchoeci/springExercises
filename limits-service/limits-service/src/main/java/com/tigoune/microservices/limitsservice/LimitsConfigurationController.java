ea8d817fdbd3ba248290b3839c5867675aebaa98
Fernando Parra <fernando.parra@globallogic.com>
2018-09-19 10:06:57 -0500

    Add Limits Service

---
+++ limits-service/limits-service/src/main/java/com/tigoune/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.tigoune.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tigoune.microservices.limitsservice.bean.LimitsConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitsConfiguration retreiveLimitsFromConfiguration() {
+		return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+}
